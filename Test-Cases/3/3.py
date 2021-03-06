9ec370d33dc7da2694940e5f3e369ab1e8c25d95
ktassenberg <69355543+ktassenberg@users.noreply.github.com>
2021-03-06 16:23:12 +0000

    Add files via upload

--- Test-Cases/3/3.py
+++ Test-Cases/3/3.py
@@ -6,4 +6,4 @@ if __name__ == "__main__":
 	output_directory = "./output/"
 	if not os.path.exists(output_directory):
 		os.mkdir(output_directory)
-	GAKTpore.analysis.run("./3-fft.tif",output_directory,80,200/260) #For the scale, 200um was measured to be 262 pixels.
+	GAKTpore.analysis.run("./3-fft.tif",output_directory,80,200/260) #For the scale, 200um was measured to be 260 pixels.
